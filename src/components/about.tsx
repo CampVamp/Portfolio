f1a7ae3d1387d51c7e45b6cb265d878c0ff592f7
CampVamp <ajayramsaravanan4@gmail.com>
2022-11-27 14:04:05 +0530

    :lipstick:Update the about-me layout

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,13 +1,22 @@
 const about = () => {
   return (
-    <div className="mt-24 flex flex-col items-center">
-      <p className="text-6xl font-bold">About Me</p>
+    <div className="m-24 flex flex-col items-center bg-zinc-800 rounded-2xl gap-8">
+      <p className="text-6xl font-bold py-10">About Me</p>
       <div className="flex">
-        <img src="../public/github.svg" alt="" className="w-12" />
+        <img src="../public/github.svg" alt="" className="w-60 m-10" />
         <div>
-          <p className="text-3xl">
-            Hi my name is Ajay. A Computer Science student intreseted in
-            Web-Development
+          <p className="text-3xl px-16 pb-24 flex-wrap">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit
+            minus eaque voluptate corrupti odit, molestiae, vitae vel totam
+            inventore earum aperiam. Quam voluptas repellendus corporis magnam,
+            quos architecto beatae labore? Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Pariatur, dignissimos praesentium
+            expedita accusantium enim quam illo maxime facere unde voluptate
+            exercitationem aliquam nulla reprehenderit, atque facilis accusamus
+            earum consequatur totam. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Natus dolorum, quasi deleniti voluptates quam sit
+            ipsa similique doloremque hic incidunt et molestiae maxime dolores
+            animi exercitationem culpa repudiandae quisquam aut!
           </p>
         </div>
       </div>
