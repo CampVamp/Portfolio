80ee10b03a3bc77ade6b1662405f7e03867da416
CampVamp <ajayramsaravanan4@gmail.com>
2022-11-28 21:56:56 +0530

    Commit that does nothing :D

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,14 +1,10 @@
-import { motion } from "framer-motion";
-
 const hero = () => {
   return (
     <div className="w-full text-black mt-24">
-      <motion.div>
-        <p className="font-nerko text-9xl text-amber-200">Hello!!</p>
-        <p className="font-nerko text-9xl text-white">
-          I'm Ajay. A sophomore CSE undergrad.
-        </p>
-      </motion.div>
+      <p className="font-nerko text-9xl text-amber-200">Hello!!</p>
+      <p className="font-nerko text-9xl text-white">
+        I'm Ajay. A sophomore CSE undergrad.
+      </p>
     </div>
   );
 };
