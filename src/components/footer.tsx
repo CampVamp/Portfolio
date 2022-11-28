f18b550875125ba5a4cabfcef81ab1679145385f
CampVamp <ajayramsaravanan4@gmail.com>
2022-11-28 14:15:53 +0530

    :bug:Fix Image 3

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,17 +5,17 @@ const footer = () => {
       <ul className="flex gap-4">
         <li className="transition-all duration-300 hover:-translate-y-1">
           <a href="https://instagram.com/_____.aj.________?igshid=ZDdkNTZiNTM=">
-            <img src="../../public/instagram.svg" alt="" className="w-14" />
+            <img src="/instagram.svg" alt="" className="w-14" />
           </a>
         </li>
         <li className="transition-all duration-300 hover:-translate-y-1">
           <a href="https://www.linkedin.com/in/ajay-ram-s-a4533a1b4">
-            <img src="../../public/linkedin.svg" alt="" className="w-14" />
+            <img src="/linkedin.svg" alt="" className="w-14" />
           </a>
         </li>
         <li className="transition-all duration-300 hover:-translate-y-1">
           <a href="https://github.com/CampVamp">
-            <img src="../../public/github.svg" alt="" className="w-14" />
+            <img src="/github.svg" alt="" className="w-14" />
           </a>
         </li>
       </ul>
